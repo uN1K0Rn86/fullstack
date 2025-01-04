0624630b6d282ac73775dd119f4cd324914d72c9
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-01-04 10:43:19 +0200

    Part 1 exercise 6

---
+++ osa1/unicafe/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const Header = () => {
+  return (
+    <h1>Give feedback</h1>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Stats = (props) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good {props.good}</p>
+      <p>Neutral {props.neutral}</p>
+      <p>Bad {props.bad}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <Header />
+      <Button handleClick={handleGoodClick} text='Good' />
+      <Button handleClick={handleNeutralClick} text='Neutral' />
+      <Button handleClick={handleBadClick} text='Bad' />
+      <Stats good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App

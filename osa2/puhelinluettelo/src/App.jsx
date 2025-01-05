c85acb16dc5dbda90538b96930c28d538d6d8ec3
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-01-05 22:53:13 +0200

    Part 2 exercise 8

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    {name: 'Arto Hellas',
+     number: '040-1234567'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -12,17 +15,23 @@ const App = () => {
       alert(`${newName} is already added to phonebook.`)
       return
     }
-    
+
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
-  const handleNameChange = (event) =>{
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -35,12 +44,17 @@ const App = () => {
                   onChange={handleNameChange} />
         </div>
         <div>
+          Number: <input
+                    value={newNumber}
+                    onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map(person =>
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>{person.name} {person.number}</p>
       )}
     </div>
   )
